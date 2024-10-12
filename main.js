5ce60f447a67ca28d27c51a26e16b2eeaaad9e38
IsThisCoding <166351231+IsThisCoding@users.noreply.github.com>
2024-10-12 13:55:17 +0000

    Added express funtionality, refactored weather script

--- main.js
+++ main.js
@@ -1,26 +1,18 @@
-const url = "https://api.open-meteo.com/v1/forecast?";
-const params = new URLSearchParams();
-params.append("latitude", "40.71");
-params.append("longitude", "-74");
-params.append("daily", "temperature_2m_max");
-params.append("temperature_unit", "fahrenheit");
-params.append("forecast_days", 1);
+const url = new URL("https://api.open-meteo.com/v1/forecast?");
+const setParams = (lat, long) => ({
+  latitude: lat,
+  longitude: long,
+  daily: "temperature_2m_max",
+  temperature_unit: "fahrenheit",
+  forecast_days: 1,
+});
+
+const params = new URLSearchParams(setParams(37.69, -122.3));
 const query_string = params.toString();
 const request_str = url + query_string;
 console.log(request_str);
 
-async function getData(url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-
-    const json = await response.json();
-    console.log(json);
-  } catch (error) {
-    console.error(error.message);
-  }
-}
-
-getData(request_str);
+fetch(request_str)
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error("Error:", error));
